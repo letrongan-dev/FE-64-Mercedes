671fc3307fd32bc9ab53b4cde4fb97a9a5c34930
letrongan-dev <57558043+letrongan-dev@users.noreply.github.com>
2021-05-15 21:30:17 +0700

    scroll change

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 window.onscroll = function (event) {
   let navbarFixed = document.querySelector(".navbar__container--sticky");
+  let welcome = document.querySelector(".welcome");
+  let category = document.querySelector(".category");
   if (window.scrollY >= 100) {
     navbarFixed.classList.remove("isScrolling");
   } else {
     navbarFixed.classList.add("isScrolling");
   }
+  if (window.scrollY >= 530) {
+    welcome.classList.remove("transtion-view");
+    welcome.classList.add("view");
+  } else if (window.scrollY <= 528) {
+    welcome.classList.remove("view");
+    welcome.classList.add("transtion-view");
+  }
+  if (window.scrollY >= 850) {
+    category.classList.remove("transtion-view");
+    category.classList.add("view");
+  } else if (window.scrollY <= 830) {
+    category.classList.remove("view");
+    category.classList.add("transtion-view");
+  }
+  //console.log(window.scrollY);
 };
